c37bbcb6f29602478edb447b8e00d2a11dba2567
JakeIss26 <jacob.issakov@gmail.com>
2026-04-25 00:34:16 +0500

    feat: add habit controller

--- src/main/java/com/example/habittracker/controller/HabitController.java
+++ src/main/java/com/example/habittracker/controller/HabitController.java
@@ -1,4 +1,29 @@
 package com.example.habittracker.controller;
 
+import com.example.habittracker.dto.request.HabitCreateRequest;
+import com.example.habittracker.dto.response.HabitResponse;
+import com.example.habittracker.service.HabitService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/habits")
+@RequiredArgsConstructor
 public class HabitController {
+
+    private final HabitService habitService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public HabitResponse createHabit(@RequestBody HabitCreateRequest request) {
+        return habitService.createHabit(request);
+    }
+
+    @GetMapping
+    public List<HabitResponse> getActiveHabits() {
+        return habitService.getActiveHabits();
+    }
 }
