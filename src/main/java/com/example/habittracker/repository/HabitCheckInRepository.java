7a5ee7a11be85e73d74167b20b716383bbe80527
JakeIss26 <jacob.issakov@gmail.com>
2026-04-24 23:46:06 +0500

    feat: add habit repositories

---
+++ src/main/java/com/example/habittracker/repository/HabitCheckInRepository.java
@@ -0,0 +1,14 @@
+package com.example.habittracker.repository;
+
+import com.example.habittracker.entity.HabitCheckIn;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface HabitCheckInRepository extends JpaRepository<HabitCheckIn, Long> {
+
+    List<HabitCheckIn> findByHabitId(Long habitId);
+
+    boolean existsByHabitIdAndCheckInDate(Long habitId, LocalDate checkInDate);
+}
--- src/main/java/com/example/habittracker/repository/HabitRepository.java
+++ src/main/java/com/example/habittracker/repository/HabitRepository.java
@@ -1,4 +1,10 @@
 package com.example.habittracker.repository;
 
-public class HabitRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.habittracker.entity.Habit;
+import java.util.List;
+
+public interface HabitRepository extends JpaRepository<Habit, Long> {
+    
+    List<Habit> findByArchivedFalse();
 }
