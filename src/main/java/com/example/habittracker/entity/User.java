29c8b3547bce1a44a342e2e085cbc43ba68dbac7
JakeIss26 <jacob.issakov@gmail.com>
2026-05-01 14:59:04 +0500

    feat: add user entity

---
+++ src/main/java/com/example/habittracker/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.habittracker.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = 50)
+    private String username;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
